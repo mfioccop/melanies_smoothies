f3d750353be415ae8bcda81c21f32396c0bf8908
Matthew Fioccoprile <mfioccoprile@gmail.com>
2024-07-11 16:34:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_df = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_df.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 ings = st.multiselect('Choose up to 5', my_df, max_selections=5)
 
 if ings:
@@ -23,7 +23,7 @@ if ings:
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fc, 'SEARCH_ON'].iloc[0]
         st.write('The search value for  ', fc, ' is ', search_on, '.')
         st.subheader(fc + ' Nutrition Information')
-        fv_resp = requests.get('https://fruityvice.com/api/fruit/' + fc)
+        fv_resp = requests.get('https://fruityvice.com/api/fruit/' + search_on)
         fv_df = st.dataframe(data=fv_resp.json(), use_container_width=True)
 
     myins = "insert into smoothies.public.orders(ingredients, name_on_order) values('" + ingst + "', '" + name + "')"
