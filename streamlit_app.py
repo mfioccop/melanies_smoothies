75f6b7c1a44858b8a8ddac4a28fb3a05aab51636
Matthew Fioccoprile <mfioccoprile@gmail.com>
2024-07-11 15:56:07 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+st.title("Customize your smoothie")
+st.write("Choose the fruits")
+
+name = st.text_input("Name of smoothie")
+st.write('Name will be', name)
+
+session = get_active_session()
+my_df = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME))
+
+ings = st.multiselect('Choose up to 5', my_df, max_selections=5)
+
+if ings:
+    ingst = ''
+    for fc in ings:
+        ingst += fc + ' '
+    myins = "insert into smoothies.public.orders(ingredients, name_on_order) values('" + ingst + "', '" + name + "')"
+    submit = st.button('Submit order')
+    if submit:
+        session.sql(myins).collect()
+        st.success('Smoothie is ordered! ' + name, icon="✔")
